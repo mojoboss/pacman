37ad4b2036c1d8759940c767d24d1f1742b46531
Mohit Joshi <animusmojo@gmail.com>
2015-08-01 19:09:35 +0530

    blit the background so that a line doesn't gets drawn.

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ while True:
 
     key_pressed = pygame.key.get_pressed() #key handling
     if key_pressed[K_UP]:
-        y -= .1
+        y -= 1
     elif key_pressed[K_DOWN]:
-        y += .1
+        y += 1
     elif key_pressed[K_LEFT]:
-        x -= .1
+        x -= 1
     elif key_pressed[K_RIGHT]:
-        x += .1
+        x += 1
 
-    pygame.draw.rect(screen, (255,255,0),[x,y,6,6])
+    background = pygame.surface.Surface((600,400)).convert()
+    background.fill((0,0,0))
+    screen.blit(background, (0,0))
+
+    pygame.draw.rect(screen, (255,255,0),[x,y,16,16])
     pygame.display.update()
