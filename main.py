3f42ecf5777ccfedcda2f2971c26b126ee004777
Mohit Joshi <animusmojo@gmail.com>
2015-08-01 21:46:56 +0530

    collisions implemented, and the projection lines removed

--- main.py
+++ main.py
@@ -2,45 +2,6 @@ __author__ = 'starlord'
 import pygame
 from pygame.locals import *
 
-pygame.init()
-screen = pygame.display.set_mode((600,400), 0, 32)
-x, y = (300, 200)
-
-while True:
-    for event in pygame.event.get(): #this will close the window
-        if event.type == QUIT:
-            exit()
-
-    key_pressed = pygame.key.get_pressed() #key handling
-    if key_pressed[K_UP]:
-        y -= 5
-    elif key_pressed[K_DOWN]:
-        y += 5
-    elif key_pressed[K_LEFT]:
-        x -= 5
-    elif key_pressed[K_RIGHT]:
-        x += 5
-
-    background = pygame.surface.Surface((600,400)).convert()
-    background.fill((0,0,0))
-    screen.blit(background, (0,0))
-
-    #large static rectangle
-    pygame.draw.rect(screen, (0,0,255),[400,200,60,80])
-
-    #lines to detect collision for static
-    pygame.draw.line(screen, (255,0,255), (0, 200), (0, 200+80), 10)
-    pygame.draw.line(screen, (255,0,255), (400, 0), (400+60, 0), 10)
-
-    #lines to detect collision for moving
-    pygame.draw.line(screen, (255,50,2), (0, y), (0, y+50), 10)
-    pygame.draw.line(screen, (255,50,2), (x, 0), (x+50, 0), 10)
-
-    #moving entity
-    pygame.draw.rect(screen, (255,255,0),[x,y,50, 50])
-    pygame.display.update()
-
-
 #-----------------------------------------------------------------------------
 #function to check collisions using SAT(separating axis theorem)
 def axis_overlap(p1, length1, p2, length2):
@@ -55,3 +16,51 @@ def axis_overlap(p1, length1, p2, length2):
         collided = True
     return collided
 #------------------------------------------------------------------------------
+
+pygame.init()
+width, height = (50,50)
+SCREEN_SIZE = (900, 600)
+screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
+x,y = (500,200)
+x2,y2,width2,height2 = (150,150,150,100)
+background = pygame.surface.Surface(SCREEN_SIZE).convert()
+background.fill((0,0,0))
+direction = 'LEFT'
+
+while True:
+    key_pressed = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    if key_pressed[K_UP]:
+        y -= 1
+        direction = 'UP'
+    elif key_pressed[K_DOWN]:
+        y += 1
+        direction = 'DOWN'
+    elif key_pressed[K_LEFT]:
+        x -= 1
+        direction = 'LEFT'
+    elif key_pressed[K_RIGHT]:
+        x += 1
+        direction = 'RIGHT'
+
+    xycollide = False
+    xcollide = axis_overlap(x,width,x2,width2)
+    ycollide = axis_overlap(y,height,y2,height2)
+    xycollide = xcollide & ycollide
+    if xycollide:
+        if direction is 'UP':
+            y = y2+height2
+        elif direction is 'DOWN':
+            y = y2-height
+        elif direction is 'LEFT':
+            x = x2+width2
+        elif direction is 'RIGHT':
+            x = x2-width
+
+    screen.blit(background, (0,0))
+    pygame.draw.rect(screen, (255,0,0),[x2,y2,width2,height2])
+    pygame.draw.rect(screen, (255,255,0),[x,y,width,height])
+    pygame.display.update()
+
