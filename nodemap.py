7ce261a6248eeb62bfd8e1ed4e1e89588b00f8f9
Mohit Joshi <animusmojo@gmail.com>
2015-08-04 22:56:57 +0530

    neighbours added and edges displayed

--- nodemap.py
+++ nodemap.py
@@ -17,6 +17,16 @@ node4 = Node((4,6), width, height)
 node5 = Node((7,6), width, height)
 node6 = Node((1,8), width, height)
 node7 = Node((4,8), width, height)
+
+node0.neighbors = [node1, node3]
+node1.neighbors = [node0, node2, node4]
+node2.neighbors = [node1]
+node3.neighbors = [node0, node4, node6]
+node4.neighbors = [node1, node3, node5, node7]
+node5.neighbors = [node4]
+node6.neighbors = [node3, node7]
+node7.neighbors = [node4, node6]
+
 nodes = [node0, node1, node2, node3, node4, node5, node6, node7]
 while True:
     for event in pygame.event.get():
@@ -25,4 +35,7 @@ while True:
     screen.blit(background, (0,0))
     for node in nodes:
         pygame.draw.circle(screen,(255,0,0),node.position.toTuple(), 10)
+    for node in nodes:
+        for nextnode in node.neighbors:
+            pygame.draw.line(screen,(255,255,255),node.position.toTuple(), nextnode.position.toTuple(), 2)
     pygame.display.update()
