d73940125f44f690bfa62e54f00cb43d41148f05
Mohit Joshi <animusmojo@gmail.com>
2015-08-08 19:13:30 +0530

    tiles graphics added to the screen

--- nodemap.py
+++ nodemap.py
@@ -4,33 +4,46 @@ import pygame
 from pygame.locals import *
 from nodegroup import NodeGroup
 from pacman import Pacman
-
+from tilegroup import Tilegroup
 from time import sleep
+
 pygame.init()
 width, height = (32, 32)
+
 screen = pygame.display.set_mode((10*width, 10*height), 0, 32)
 background = pygame.surface.Surface((10*width,10*height)).convert()
 background.fill((0,0,0))
+filename = 'nodemap_test.txt'
+
+tilegrp = Tilegroup(width, height)
+tiles = tilegrp.createTileList(filename)
 
 nodegrp = NodeGroup(width, height)
-nodes = nodegrp.createNodeList('nodemap_test.txt')
-pacman = Pacman((width/2,height/2), [nodes[3].position.x, nodes[3].position.y])
-'''
-for n in nodes:
-    print n.position.x
-'''
+nodes = nodegrp.createNodeList(filename)
+
+pacman = Pacman((width,height), [nodes[3].position.x, nodes[3].position.y])
+
 while True:
     #sleep(0.01)
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
     screen.blit(background, (0,0))
+
+    for t in tiles:
+        t.draw(screen)
+
+    pacman.draw(screen)
+    pacman.update(nodes)
+
+    pygame.display.update()
+
+
+'''
+#code to draw the node graph
     for node in nodes:
         pygame.draw.circle(screen,(255,0,0),node.position.toTuple(), 10)
     for node in nodes:
         for nextnode in node.neighbors:
             pygame.draw.line(screen,(255,255,255),node.position.toTuple(), nextnode.position.toTuple(), 2)
-    pacman.draw(screen)
-    pacman.update(nodes)
-
-    pygame.display.update()
+'''
---
+++ tilegroup.py
@@ -0,0 +1,19 @@
+__author__ = 'starlord'
+import numpy
+from tiles import  Tile
+class Tilegroup(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tilelist = []
+
+    def createTileList(self, filename):
+        layout = numpy.loadtxt(filename, dtype=str)
+        col, row = layout.shape
+        for i in range(row):
+            for j in range(col):
+                value = layout[j][i]
+                if (value == '0'):
+                    pos = (i*self.width, j*self.height)
+                    self.tilelist.append(Tile((self.width, self.height), pos))
+        return self.tilelist
