73fe20c87fc6728d65660c681ac46aae283f498d
Mohit Joshi <animusmojo@gmail.com>
2015-08-09 21:54:37 +0530

    locate pacman file added which locates pacman using dfs

---
+++ locatePacman.py
@@ -0,0 +1,20 @@
+__author__ = 'starlord'
+import Queue
+#this method runs dfs on nodes graph and returns a python map of the form
+#'map[child] = parent'
+def search_pacnode(ghostnode, pacnode):
+    q = Queue.Queue()
+    q.put(ghostnode)
+    map = {}
+    explored = []
+    while not q.empty():
+        node = q.get()
+        explored.append(node)
+        if(node == pacnode):
+            return map
+        else:
+            for n in node.neighbors:
+                if n not in explored:
+                    q.put(n)
+                    map[n] = node
+
